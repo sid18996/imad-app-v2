5114ddbdb3a409674396da7504b8fcb2a0c5ba95
sid18996 <jsiddharth1008@gmail.com>
2017-02-06 00:15:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -94,12 +94,6 @@ app.get('/:articleName',function(req,res){
     var articleName=req.params.articleName;
      res.send(creatTemplate(articles[articleName]));
 });
-/*app.get('/article-two',function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
-app.get('/article-three',function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
-});*/
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
