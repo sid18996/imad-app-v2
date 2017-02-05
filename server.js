378cb5e18af246136bb93fbaaeb47f4bba2d5455
sid18996 <jsiddharth1008@gmail.com>
2017-02-05 20:59:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,10 +16,10 @@ app.get('/article-one',function(req,res){
      res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 app.get('/article-two',function(req,res){
-    res.send('Article two is requested and will be searved');
+    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 app.get('/article-three',function(req,res){
-    res.send('Article three is requested and will be searved');
+    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
