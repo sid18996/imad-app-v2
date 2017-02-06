ef98ecbdb2a3cd18c7610a4bc3df71f7ec38a559
sid18996 <jsiddharth1008@gmail.com>
2017-02-06 20:30:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,3 +4,13 @@ console.log('Loaded!');
 
 var element= document.getElementById( 'main-text');
 element.innerHTML = 'New Value';
+
+
+// Move the image
+  
+  
+var madi= document.getElementById('madi');
+  madi.onclick= function(){
+    madi.style.marginLeft= '100px';
+  };
+    
