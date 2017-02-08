5e72862bac7a9d82fe03adecc065431562057721
sid18996 <jsiddharth1008@gmail.com>
2017-02-08 23:36:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,26 +1,26 @@
 // Counter code 
 var button = document.getElmentById('button');
 button.onclick = function(){
-// Creat a request
-var request= new XMLHttpRequest();
+     // Creat a request
+    var request= new XMLHttpRequest();
 
-//Make a request
-request.open('GET', 'htttp://http://sid18996.imad.hasura-app.io/counter', true);
-request.send(null);
+    //Make a request
+    request.open('GET', 'htttp://http://sid18996.imad.hasura-app.io/counter', true);
+    request.send(null);
 
-//Capture he responce ina variable
-request.onreadystatechange = function(){
-    if(request.readytState == XMLHttpRequest.DONE){
-    //Take some action
-        if(request.status == 200)
-        {
-             var counter= request.responseText;
-             var span= document.getElementById('count');
-             span.innerHTML = counter.toString();
+    //Capture he responce ina variable
+    request.onreadystatechange = function(){
+        if(request.readytState == XMLHttpRequest.DONE){
+        //Take some action
+            if(request.status == 200)
+            {
+                 var counter= request.responseText;
+                 var span= document.getElementById('count');
+                 span.innerHTML = counter.toString();
+            }
         }
-    }
-    // No do noting  
-};
+        // No do noting  
+    };
     
 };
 
