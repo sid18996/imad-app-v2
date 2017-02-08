18d3275795b4b9cea288a4bfd5437c6266fed70e
sid18996 <jsiddharth1008@gmail.com>
2017-02-08 21:30:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@
 var button = document.getElmentById('button');
 
 // Creat a request
-var request= new XMLHttprequest();
+var request= new XMLHttpRequest();
 
 //Make a request
 request.open('GET', 'htttp://http://sid18996.imad.hasura-app.io/counter', true);
@@ -10,7 +10,7 @@ request.send(null);
 
 //Capture he responce ina variable
 request.onreadystatechange = function(){
-    if(request.readtState == XMLHttprequest.DONE){
+    if(request.readytState == XMLHttpRequest.DONE){
     //Take some action
         if(request.status == 200)
         {
