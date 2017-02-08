2eb9b2b59b17567bce8d3453846e7ba0f85796e5
sid18996 <jsiddharth1008@gmail.com>
2017-02-08 21:27:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,7 +15,7 @@ request.onreadystatechange = function(){
         if(request.status == 200)
         {
              var counter= request.responseText;
-             var span document.getElementById('count');
+             var span= document.getElementById('count');
              span.innerHTML = counter.toString();
         }
     }
