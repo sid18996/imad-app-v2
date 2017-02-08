22f5fbad2e3929a0b490c2e38ce371636c5d1165
sid18996 <jsiddharth1008@gmail.com>
2017-02-08 20:53:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,12 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter=0;
+app.get('/counter', function(req,res){
+    counter = counter +1;
+   res.send(counter.toString()); 
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
@@ -95,11 +101,7 @@ app.get('/:articleName',function(req,res){
      res.send(creatTemplate(articles[articleName]));
 });
 
-var counter=0;
-app.get('/counter', function(req,res){
-    counter = counter +1;
-   res.send(counter.toString()); 
-});
+
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
