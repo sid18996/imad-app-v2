acf4b137e00e3bdc833bd13ad05fd5e631e013e4
sid18996 <jsiddharth1008@gmail.com>
2017-02-11 00:13:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -32,13 +32,34 @@ var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
     //Make request to the server
     //Capture all list of name
-    var name= ['name1','name2','name3','name4'];
-    var list = '';
-    for(var i=0;i<name.length;i++){
-        list += '<li>'+ name[i]+'</li>';
-    }
-    var ul= document.getElementById('namelist');
-    ul.innerHTML = list;
+      // Creat a request
+    var request= new XMLHttpRequest();
+
+    //Capture he responce ina variable
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE){
+        //Take some action
+            if(request.status === 200)
+            {
+                var names = request.responseText;
+                names=JSON.parse(names);
+                var list = '';
+                for(var i=0;i<name.length;i++){
+                    list += '<li>'+ name[i]+'</li>';
+                }
+                var ul= document.getElementById('namelist');
+                ul.innerHTML = list;
+   
+            }
+        }
+        // No do noting  
+    };
+    
+    //Make a request to counter end point
+    request.open('GET', 'http://sid18996.imad.hasura-app.io/submit-name/name='+ name, true);
+    request.send(null);
+
+    
     
 };
 
