6a7d236aa98e240dbb9a4732feb0009e8b6e8f1f
sid18996 <jsiddharth1008@gmail.com>
2017-02-10 20:06:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,7 +19,7 @@ button.onclick = function(){
     };
     
     //Make a request to counter end point
-    request.open('GET', 'htttp://http://sid18996.imad.hasura-app.io/counter', true);
+    request.open('GET', '//http://sid18996.imad.hasura-app.io/counter', true);
     request.send(null);
 
 };
