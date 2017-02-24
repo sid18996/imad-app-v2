9560653a34fcb3c8dfed5cee516d32d1d3e43e8c
sid18996 <jsiddharth1008@gmail.com>
2017-02-24 12:35:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var config = {
     database : 'sid18996',
     host : 'db.imad.hasura-app.io.',
     port : '5432',
-    password : process.env.DB_PASSEORD
+    password : process.env.DB_PASSWORD
 };
 
 
