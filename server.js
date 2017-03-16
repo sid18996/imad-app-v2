7fafca5e30277976becc790ce696287a1560da87
sid18996 <jsiddharth1008@gmail.com>
2017-03-16 12:45:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.get('/', function (req, res) {
 function hash(input , salt){
      //jow do we creat hash?
      var hashed = crypto.pbkdf2Sync(input , salt,10000, 512 ,'sha512');
-     return hashed,toString('hex');
+     return hashed.toString('hex');
  }
  
 app.get('/hash/:input', function(req,res){
