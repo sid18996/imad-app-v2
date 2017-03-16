1855f10a1265b1aedaaa997fce2a12d851124f5f
sid18996 <jsiddharth1008@gmail.com>
2017-03-16 12:42:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -20,6 +20,21 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+
+function hash(input , salt){
+     //jow do we creat hash?
+     var hashed = crypto.pbkdf2Sync(input , salt,10000, 512 ,'sha512');
+     return hashed,toString('hex');
+ }
+ 
+app.get('hash/:input', function(req,res){
+    var hashedString = hash(req.params.input , 'this-is-some-random-string');
+    res.send(hashedString);
+});
+ 
+
+
+
 var pool = new Pool(config)
 app.get('/test-db', function(req, res){
     // make a select request
@@ -120,16 +135,6 @@ function createTemplate(data)
      return htmlTemplate;
  }
  
- function hash(input , salt){
-     //jow do we creat hash?
-     var hashed = crypto.pbkdf2Sync(input , salt,10000, 512 ,'sha512');
-     return hashed,toString('hex');
- }
- 
-app.get('hash/:input', function(req,res){
-    var hashedString = hash(req.params.input , 'this-is-some-random-string');
-    res.send(hashedString);
-});
  
 
 var names =[];
